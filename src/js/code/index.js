3bde178d8136e58c4f283e39dc4d05dc21a47e8d
Oleksandr <smiyanaleksandr@gmail.com>
2023-06-29 21:13:10 +0300

    created loading movies on initial page load

--- src/js/code/index.js
+++ src/js/code/index.js
@@ -1,29 +1,37 @@
 
-import {clickTitleSlider} from './clickTitleSlider'
-import {slider} from './slider'
-import { getReqPageOne} from './showNewMovie'
-import {showSelect} from './listeren'
-export function startApp () {
+import { clickTitleSlider } from './clickTitleSlider'
+import { slider } from './slider'
+import { getReqPageOne } from './showNewMovie'
+import { showSelect } from './listeren'
+import { getMovies } from './moviesOnPageLoad'
 
-  document.addEventListener('DOMContentLoaded',()=>{
 
+
+
+export function startApp() {
+
+  document.addEventListener('DOMContentLoaded', () => {
     // 
     getReqPageOne()
-  
+
+    // movies on page load
+    getMovies()
     
+
+
     // 
     showSelect();
 
 
-      // Switching pages within the 'New' section: Movies, TV shows, Cartoons, Anime.
-clickTitleSlider()
+    // Switching pages within the 'New' section: Movies, TV shows, Cartoons, Anime.
+    clickTitleSlider()
 
 
-// Slider
+    // Slider
 
-slider()
-    
-})
+    slider()
+
+  })
 }
 
 
